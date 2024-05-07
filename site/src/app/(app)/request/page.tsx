ea0d2b498cb9dff764b3b20818ac58c38bed0326
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-07 09:20:49 +0700

    fix lint

--- site/src/app/(app)/request/page.tsx
+++ site/src/app/(app)/request/page.tsx
@@ -20,7 +20,7 @@ export default function RequestPage() {
         I only have time to work on this project during the weekends, so if you have any feature or logo requests, I will appreciate it if you can create an issue on the respective GitHub repository.
 
         <h3 className="pt-4 text-theme-stronger text-lg">Want to add your logo here?</h3>
-        <p className="">You can reach out to <a className="" href="https://github.com/alfonsusac/kawaii-logos-data">kawaii-logos-data's GitHub repository</a> to <a href="https://github.com/alfonsusac/kawaii-logos-data/issues">create an issue</a> and let me know the link to your github repository or just any kinds of links that has transparent images.</p>
+        <p className="">You can reach out to <a className="" href="https://github.com/alfonsusac/kawaii-logos-data">kawaii-logos-data&apos;s GitHub repository</a> to <a href="https://github.com/alfonsusac/kawaii-logos-data/issues">create an issue</a> and let me know the link to your github repository or just any kinds of links that has transparent images.</p>
 
         <p>If you have a collection of images that are stored in GitHub, I can also help you to automate the process of adding your images to the website. Just let me know the link to your repository and I will take care of the rest.</p>
 
@@ -28,7 +28,7 @@ export default function RequestPage() {
         
         <h3 className="pt-4 text-theme-stronger text-lg">Improvement to the site?</h3>
 
-        <p className="">If you have any feature requests for this website please head out to <a className="" href="https://github.com/alfonsusac/service-title-logo">service-title-logo's GitHub repository</a> instead and <a className="https://github.com/alfonsusac/service-title-logo/issues">create an issue</a> there. Its different, one is for the front-end, and the other is for the data collection.</p>
+        <p className="">If you have any feature requests for this website please head out to <a className="" href="https://github.com/alfonsusac/service-title-logo">service-title-logo&apos;s GitHub repository</a> instead and <a className="https://github.com/alfonsusac/service-title-logo/issues">create an issue</a> there. Its different, one is for the front-end, and the other is for the data collection.</p>
 
       </div>
     </>
