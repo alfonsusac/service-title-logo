2606a612ef56f771a41fdf0063e2d3624731aa58
Alfonsus Ardani <alfonsusac@gmail.com>
2024-05-20 00:09:30 +0700

    boudnary aware dropdown

--- site/src/app/(app)/ThemeChanger.tsx
+++ site/src/app/(app)/ThemeChanger.tsx
@@ -30,6 +30,8 @@ export function ThemeDropdown() {
           shadow-xl
           shadow-black/20
           min-w-48
+          max-h-[var(--radix-dropdown-menu-content-available-height)]
+          overflow-y-auto
           "
         >
           <DropdownMenuArrow width={30} height={10} className="fill-theme-card"/>
@@ -38,8 +40,9 @@ export function ThemeDropdown() {
               <DropdownMenuItem
                 onClick={() => setTheme(itheme)}
                 key={i}
-                className=
-                "p-3 -m-1 hover:bg-theme-cardHover outline-none rounded-lg cursor-pointer data-[selected=true]:bg-theme-cardHover data-[selected=true]:text-theme-strong"
+                className="p-3 -m-1 hover:bg-theme-cardHover outline-none rounded-lg cursor-pointer data-[selected=true]:bg-theme-cardHover data-[selected=true]:text-theme-strong 
+                flex-none
+                "
                 data-selected={theme === itheme}
               >
                 {itheme}
@@ -58,3 +61,8 @@ export function MaterialSymbolsPalette(props: SVGProps<SVGSVGElement>) {
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M12 22q-2.05 0-3.875-.788t-3.187-2.15t-2.15-3.187T2 12q0-2.075.813-3.9t2.2-3.175T8.25 2.788T12.2 2q2 0 3.775.688t3.113 1.9t2.125 2.875T22 11.05q0 2.875-1.75 4.413T16 17h-1.85q-.225 0-.312.125t-.088.275q0 .3.375.863t.375 1.287q0 1.25-.687 1.85T12 22m-5.5-9q.65 0 1.075-.425T8 11.5t-.425-1.075T6.5 10t-1.075.425T5 11.5t.425 1.075T6.5 13m3-4q.65 0 1.075-.425T11 7.5t-.425-1.075T9.5 6t-1.075.425T8 7.5t.425 1.075T9.5 9m5 0q.65 0 1.075-.425T16 7.5t-.425-1.075T14.5 6t-1.075.425T13 7.5t.425 1.075T14.5 9m3 4q.65 0 1.075-.425T19 11.5t-.425-1.075T17.5 10t-1.075.425T16 11.5t.425 1.075T17.5 13"></path></svg>
   )
 }
+
+// max-h-[var(--radix-dropdown-menu-content-available-height)]
+// overflow-y-auto
+
+// flex-none
