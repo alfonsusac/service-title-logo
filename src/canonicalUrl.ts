0955b5320f215bd331b33d13e19dc1abc4f5eddb
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-13 13:38:31 +0700

    fix canonical url

--- src/canonicalUrl.ts
+++ src/canonicalUrl.ts
@@ -1,6 +1,6 @@
 export const canonicalUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000" as const
-    : process.env.VERCEL_URL
-      ? `https://${ process.env.VERCEL_URL }` as const
+    : process.env.VERCEL_PROJECT_PRODUCTION_URL
+      ? `https://${ process.env.VERCEL_PROJECT_PRODUCTION_URL }` as const
       : `https://kawaiilogos.alfon.dev` as const
