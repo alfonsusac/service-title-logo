2b0eb430618695fae779c222c60d74c325100fed
Alfonsus Ardani <alfonsusac@gmail.com>
2026-05-13 11:55:15 +0700

    upate revalidate route requirement. added revalidateion docs

--- src/app/(app)/revalidate/route.ts
+++ src/app/(app)/revalidate/route.ts
@@ -4,13 +4,35 @@ import { NextRequest } from "next/server"
 
 export const dynamic = 'force-dynamic'
 
-export function GET(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams
-  if (process.env.NODE_ENV !== "development" && searchParams.get('key') !== process.env.REVALIDATE_KEY)
-    return redirect('/')
+export function GET() {
+  return new Response([
+    "Revalidate Route for Kawaii Logos.",
+    "",
+    "",
+    "Routes:",
+    "",
+    " - GET  /revalidate",
+    "     Shows this page",
+    "",
+    " - POST /revalidate",
+    "     Revalidates all routes and tags, requires a token for authentication",
+    "     {",
+    "       token: string      // must match process.env.REVALIDATE_KEY",
+    "     }",
+  ].join('\n'), { status: 200 })
+}
+
+export async function POST(request: NextRequest) {
+  const json = await request.json()
+  // In development, we allow revalidation without a token for easier testing.
+  if (process.env.NODE_ENV !== "development") {
+    if (!json.token)
+      return new Response('Unauthorized', { status: 401 })
+    if (json.token !== process.env.REVALIDATE_KEY)
+      return new Response('Unauthorized', { status: 401 })
+  }
   revalidatePath('/')
   revalidateTag('all', "max")
-  // updateTag('all')
   return Response.json({
     message: 'Revalidated all routes and tags.'
   })
